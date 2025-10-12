7d4b892208b66760a892f70004f9929746e48bb6
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-10-12 18:53:41 +0530

    FE+BE connection with auth practice1

---
+++ week 6/6.2 - Auth and Connecting FE to BE/Practice/index.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = "YUVI"
+const app = express()
+
+app.use(express.json())
+
+const users= []
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.post("/signup",(req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
+    users.push({
+        username:username,
+        password:password
+    })
+    res.json({message:"successfully signed up"})
+})
+
+app.post("/signin",(req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    const verifyUsers = users.find( function(u){
+        if(u.username == username && u.password == password){
+            return true
+        }
+        else{
+            return false
+        }
+    })
+
+    if(!verifyUsers){
+        res.status(404).send({message:"invalid user"})
+    } else {
+        const token = jwt.sign({username:username},JWT_SECRET)
+        res.send({
+            message:"successfully signed in",
+            user:username,
+            pwd:password,
+            jwt_token:token
+        })
+    }
+})
+
+function auth(req,res,next){
+const token = req.headers.authorization
+  if(!token){
+   return res.json({message:"token is missing"})
+  } 
+  try{
+     const verifytoken = jwt.verify(token,JWT_SECRET) ;
+     req.username = verifytoken.username
+     next()
+  }
+   
+
+ catch(err) {
+        res.status(401).json({message:"you are not logged in"})
+    }
+  
+  
+}
+app.get("/me",auth,(req,res)=>{
+    const finduser = users.find((user)=>user.username == req.username)
+  if(finduser){
+    return res.json({username :finduser.username , password : finduser.password})
+  }else{
+    res.json({message:"invalid token"})
+  }
+})
+
+app.listen(3000)
