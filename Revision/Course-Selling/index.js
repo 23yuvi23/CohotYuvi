ca2502994052a016511b140e5ec299ee699ad9b0
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-12-21 10:35:09 +0530

    step-2 Add skeleton routes

--- Revision/Course-Selling/index.js
+++ Revision/Course-Selling/index.js
@@ -1,2 +1,38 @@
-//try
-let firstname = "yuvi"
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.post('/user/signup', (req, res) => {
+  res.json({
+    message:"signup endpoint"
+  })
+})
+
+app.post('/user/signin', (req, res) => {
+  res.json({
+    message:"signin endpoint"
+  })
+})
+
+app.get('/user/purchases', (req, res) => {
+  res.json({
+    message:"purchases endpoint"
+  })
+})
+
+app.post('/course/purchase', (req, res) => {
+  res.json({
+    message:"course to be purchase endpoint"
+  })
+})
+
+app.get('/courses', (req, res) => {
+  res.json({
+    message:"all courses endpoint"
+  })
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
