4b794fa3aaa4dc09655fef1f87d8c5648ab70fb2
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-12-23 08:28:19 +0530

    created admin Route

---
+++ Revision/Course-Selling/routes/admin.js
@@ -0,0 +1,36 @@
+const { Router } = require ("express")
+const adminRouter = Router();
+
+// asdminRouter.use(adminMiddleware)
+adminRouter.post('/signup', (req, res) => {
+  res.json({
+    message:"signup endpoint"
+  })
+})
+
+adminRouter.post('/signin', (req, res) => {
+  res.json({
+    message:"signin endpoint"
+  })
+})
+//create a course
+adminRouter.post('/course', (req, res) => {
+  res.json({
+    message:"create course endpoint"
+  })
+})
+//change name price image of course
+adminRouter.post('/course', (req, res) => {
+  res.json({
+    message:"change details endpoint"
+  })
+})
+//get all their course in bulk
+adminRouter.post('/course/bulk', (req, res) => {
+  res.json({
+    message:"get all course endpoint"
+  })
+})
+module.exports = {
+    adminRouter:adminRouter
+}
