318172aa4e9daef5c2b30da7591717080efa9e4d
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-12-26 11:54:45 +0530

    react componet creation

--- week 9-react/9.3 - React From Basic Part 1 (Recorded)/Lecture Codes/01_create-react-app/vite-project/src/App.jsx
+++ week 9-react/9.3 - React From Basic Part 1 (Recorded)/Lecture Codes/01_create-react-app/vite-project/src/App.jsx
@@ -2,8 +2,47 @@ import './App.css'
 
 function App() {
   return (
-    <div>hi there</div>
+    <div style={{background:"#dfe6e9",height:"100vh",}}>
+      <div style={{display:"flex",justifyContent :"center"}}>
+      <PostComponent />
+      <PostComponent />
+      <PostComponent />
+      </div>
+    </div>    
   )
 }
 
-export default App
+const style = {width:200, backgroundColor: "white", borderRadius: 10, borderColor: "gray" , borderWidth: 1, 
+padding: 20 }
+function PostComponent(){
+  return(
+    <div style={style}>
+    <div style={{display:"flex"}}> 
+    <img src={"https://img.pikbest.com/png-images/20241022/stealth-masked-hacker-gaming-logo-for-gamers_10991543.png!bw700"} 
+    alt="image here" 
+    style={{
+      width:30,
+      height:30,
+      borderRadius:20
+    }}
+    />
+    <div style={{fontSize:10,marginLeft:10}}>
+      <b>
+      100xdevs
+      </b>
+      
+      <div>23,888 followers </div>
+      <div>12m </div>
+    </div>
+
+    </div>
+    <div style={{fontSize:12}}>
+      want to know how to win big ? Check out how theese folks won $6000 in bounties.
+    </div>
+    </div>
+
+  )
+}
+
+export default App 
+
