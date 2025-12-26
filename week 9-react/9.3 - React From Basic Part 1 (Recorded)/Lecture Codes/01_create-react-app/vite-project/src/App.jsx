ebf4121c0cd0af181c28943acf2eda4b9d51a0ea
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-12-26 12:40:55 +0530

    component2

--- week 9-react/9.3 - React From Basic Part 1 (Recorded)/Lecture Codes/01_create-react-app/vite-project/src/App.jsx
+++ week 9-react/9.3 - React From Basic Part 1 (Recorded)/Lecture Codes/01_create-react-app/vite-project/src/App.jsx
@@ -3,10 +3,10 @@ import './App.css'
 function App() {
   return (
     <div style={{background:"#dfe6e9",height:"100vh",}}>
-      <div style={{display:"flex",justifyContent :"center"}}>
-      <PostComponent />
-      <PostComponent />
-      <PostComponent />
+    {/* <div style={{}}> */}
+      <div style={{display:"flex",justifyContent :"center",alignItems:"center"}}>
+      {/* <PostComponent /> */}
+      <ProfileCard/>  
       </div>
     </div>    
   )
@@ -14,6 +14,8 @@ function App() {
 
 const style = {width:200, backgroundColor: "white", borderRadius: 10, borderColor: "gray" , borderWidth: 1, 
 padding: 20 }
+// const style2 = {width:}
+
 function PostComponent(){
   return(
     <div style={style}>
@@ -41,6 +43,36 @@ function PostComponent(){
     </div>
     </div>
 
+  )
+}
+
+function ProfileCard (){
+  return (
+    <div id='superparent' style={{style}}>
+    <div style={{display:"flex",flexDirection: "column",alignItemst:'center'}}> 
+    <img src={"https://img.pikbest.com/png-images/20241022/stealth-masked-hacker-gaming-logo-for-gamers_10991543.png!bw700"} 
+    alt="image here" 
+    style={{
+      width:60,
+      height:60,
+      borderRadius:10,
+      marginBottom:10
+    }}
+      />
+      <b>Harkirat Singh</b>
+      <span>Working with WebRTC</span>
+    </div>
+    <div style={{ fontSize: 14, marginTop: 25 }}>
+                <p style={{display: "flex", justifyContent: "space-between"}}>
+                    <span>Profile Viewers </span>
+                    <span style={{color: "blue"}}>41,903</span>
+                </p>
+                <p style={{display: "flex", justifyContent: "space-between"}}>
+                    <span>Post Impressions</span>
+                    <span style={{color: "blue"}}>1,313</span>
+                </p>
+            </div>
+    </div>
   )
 }
 
