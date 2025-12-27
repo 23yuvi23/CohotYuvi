d9d62aebf03160d062696a7a7b67716dd815217e
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-12-27 09:36:39 +0530

    made 2 components seperately

--- week 9-react/9.3 - React From Basic Part 1 (Recorded)/Lecture Codes/01_create-react-app/vite-project/src/App.jsx
+++ week 9-react/9.3 - React From Basic Part 1 (Recorded)/Lecture Codes/01_create-react-app/vite-project/src/App.jsx
@@ -1,80 +1,30 @@
-import './App.css'
+import PostComponent from "./PostComponent";
+import ProfileComponent from "./ProfileComponent";
 
+// Create a function component named App that will be rendered in the root element
 function App() {
-  return (
-    <div style={{background:"#dfe6e9",height:"100vh",}}>
-    {/* <div style={{}}> */}
-      <div style={{display:"flex",justifyContent :"center",alignItems:"center"}}>
-      {/* <PostComponent /> */}
-      <ProfileCard/>  
-      </div>
-    </div>    
-  )
-}
+    // return JSX that will be rendered
+    return (
+        // Apply inline styles to the div element
+        <div style={{ backgroundColor: "#dfe6e9", minHeight: "100vh" }}>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <div>
+                    {/* Call PostComponent here to render it in the App component */}
+                    <PostComponent />
 
-const style = {width:200, backgroundColor: "white", borderRadius: 10, borderColor: "gray" , borderWidth: 1, 
-padding: 20 }
-// const style2 = {width:}
-
-function PostComponent(){
-  return(
-    <div style={style}>
-    <div style={{display:"flex"}}> 
-    <img src={"https://img.pikbest.com/png-images/20241022/stealth-masked-hacker-gaming-logo-for-gamers_10991543.png!bw700"} 
-    alt="image here" 
-    style={{
-      width:30,
-      height:30,
-      borderRadius:20
-    }}
-    />
-    <div style={{fontSize:10,marginLeft:10}}>
-      <b>
-      100xdevs
-      </b>
-      
-      <div>23,888 followers </div>
-      <div>12m </div>
-    </div>
-
-    </div>
-    <div style={{fontSize:12}}>
-      want to know how to win big ? Check out how theese folks won $6000 in bounties.
-    </div>
-    </div>
-
-  )
-}
-
-function ProfileCard (){
-  return (
-    <div id='superparent' style={{style}}>
-    <div style={{display:"flex",flexDirection: "column",alignItemst:'center'}}> 
-    <img src={"https://img.pikbest.com/png-images/20241022/stealth-masked-hacker-gaming-logo-for-gamers_10991543.png!bw700"} 
-    alt="image here" 
-    style={{
-      width:60,
-      height:60,
-      borderRadius:10,
-      marginBottom:10
-    }}
-      />
-      <b>Harkirat Singh</b>
-      <span>Working with WebRTC</span>
-    </div>
-    <div style={{ fontSize: 14, marginTop: 25 }}>
-                <p style={{display: "flex", justifyContent: "space-between"}}>
-                    <span>Profile Viewers </span>
-                    <span style={{color: "blue"}}>41,903</span>
-                </p>
-                <p style={{display: "flex", justifyContent: "space-between"}}>
-                    <span>Post Impressions</span>
-                    <span style={{color: "blue"}}>1,313</span>
-                </p>
+                    {/* Call ProfileComponent here to render it in the App component */}
+                    <ProfileComponent />
+                </div>
             </div>
-    </div>
-  )
+        </div>
+    );
 }
 
-export default App 
-
+// Export App Component to use it in other components
+export default App;
