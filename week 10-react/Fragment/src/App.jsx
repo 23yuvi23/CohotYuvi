4c505b281160868d489ced1f5f6d9bed5f886501
23yuvi23 <yogeshpaliwal9493@gmail.com>
2026-04-13 13:30:20 +0530

    fragment  <> </>

---
+++ week 10-react/Fragment/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    // <> is fragment  so extra div na aaye 
+    <>
+      i am parent div
+      <div>i am div 1</div>
+      <div>i am div 2 </div>
+    </>
+    
+  )
+}
+
+export default App
