49de628dd0bbf13de207c527e000df59400d807c
23yuvi23 <yogeshpaliwal9493@gmail.com>
2026-04-16 16:52:06 +0530

    SPA and routing done

--- week 10b-react/SinglePageApplication/src/App.jsx
+++ week 10b-react/SinglePageApplication/src/App.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
-import {BrowserRouter,Routes,Route,Link,useNavigate, redirect} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link, useNavigate, redirect } from "react-router-dom"
 
 const App = () => {
   return (
     <div>
-      
+
       <BrowserRouter>
-      <Link to="/">Home</Link>  || <Link to="/user1">User1</Link>  || <Link to="/user2">User2</Link> 
+        <Link to="/">Home</Link>  || <Link to="/user1">User1</Link>  || <Link to="/user2">User2</Link>
         <Routes>
-          <Route path = "/"  element = {<Home />} />
-          <Route path = "/user1"  element = {<User1 />} />
-          <Route path = "/user2"  element = {<User2 />} />
-          <Route path = "*"  element = {<ErrorPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/user1" element={<User1 />} />
+          <Route path="/user2" element={<User2 />} />
+          <Route path="*" element={<ErrorPage />} />
 
         </Routes>
       </BrowserRouter>
@@ -19,31 +19,31 @@ const App = () => {
   )
 }
 
-function Home () {
+function Home() {
   return <div>
     Welcome to Home Page
   </div>
 }
-function User1 () {
+function User1() {
   return <div>
-    Hello User 1 welcome 
+    Hello User 1 welcome
   </div>
 }
 
-function ErrorPage () {
+function ErrorPage() {
   return <div>
     Sorry Page not found
   </div>
 }
-function User2 () {
+function User2() {
   // using hook want user to go back to / page after 10 sec 
-  const navigate  = useNavigate();
+  const navigate = useNavigate();
 
   function redirectUser() {
     navigate('/')
   }
   return <div>
-    Hello User 2 welcome 
+    Hello User 2 welcome
     <button onClick={redirectUser}>Go back to landing page </button>
   </div>
 }
